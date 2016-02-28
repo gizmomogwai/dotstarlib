9c601065939c8ab68f908802c746666a55f72885
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-02-28 16:22:03 +0100

    reformat

--- src/lib/cpp/dotstarlib.h
+++ src/lib/cpp/dotstarlib.h
@@ -25,74 +25,74 @@ class Exception {
 
 
 #include <sys/types.h>
-  #include <sys/stat.h>
-  #include <fcntl.h>
-  #include <sys/ioctl.h>
-  #include <linux/spi/spidev.h>
-  #include <cstring>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <cstring>
 
-  class Spi {
-  public:
+class Spi {
+ public:
   Spi() {
-  ::memset(fXfer, 0, sizeof(fXfer));
-  std::cout << sizeof(fXfer) << std::endl;
-  fXfer[0].tx_buf = 0;
-  fXfer[0].rx_buf = 0;
-  fXfer[0].len = 4;
-  fXfer[0].delay_usecs = 0;
-  fXfer[0].bits_per_word = 8;
-  fXfer[0].cs_change = 0;
-  fXfer[1].rx_buf = 0;
-  fXfer[1].delay_usecs = 0;
-  fXfer[1].bits_per_word = 8;
-  fXfer[1].cs_change = 0;
-  fXfer[2].tx_buf = 0;
-  fXfer[2].rx_buf = 0;
-  fXfer[2].delay_usecs = 0;
-  fXfer[2].bits_per_word = 8;
-  fXfer[2].cs_change = 0;
+    ::memset(fXfer, 0, sizeof(fXfer));
+    std::cout << sizeof(fXfer) << std::endl;
+    fXfer[0].tx_buf = 0;
+    fXfer[0].rx_buf = 0;
+    fXfer[0].len = 4;
+    fXfer[0].delay_usecs = 0;
+    fXfer[0].bits_per_word = 8;
+    fXfer[0].cs_change = 0;
+    fXfer[1].rx_buf = 0;
+    fXfer[1].delay_usecs = 0;
+    fXfer[1].bits_per_word = 8;
+    fXfer[1].cs_change = 0;
+    fXfer[2].tx_buf = 0;
+    fXfer[2].rx_buf = 0;
+    fXfer[2].delay_usecs = 0;
+    fXfer[2].bits_per_word = 8;
+    fXfer[2].cs_change = 0;
 
-  const char* SPI_DEV = "/dev/spidev0.0";
-  fFile = open(SPI_DEV, O_RDWR);
-  if (fFile < 0) {
-  std::ostringstream s;
-  s << "cannot open '";
-  s << SPI_DEV;
-  s << "'";
-  throw new Exception(s.str());
-  }
-  uint8_t mode = SPI_MODE_0 | SPI_NO_CS;
-  int res = ::ioctl(fFile, SPI_IOC_WR_MODE, &mode);
-  if (res != 0) {
-  throw new Exception("ioctl SPI_IOC_WR_MODE failed with error: ", res);
-  }
+    const char* SPI_DEV = "/dev/spidev0.0";
+    fFile = open(SPI_DEV, O_RDWR);
+    if (fFile < 0) {
+      std::ostringstream s;
+      s << "cannot open '";
+      s << SPI_DEV;
+      s << "'";
+      throw new Exception(s.str());
+    }
+    uint8_t mode = SPI_MODE_0 | SPI_NO_CS;
+    int res = ::ioctl(fFile, SPI_IOC_WR_MODE, &mode);
+    if (res != 0) {
+      throw new Exception("ioctl SPI_IOC_WR_MODE failed with error: ", res);
+    }
 
-  res = ioctl(fFile, SPI_IOC_WR_MAX_SPEED_HZ, BITRATE);
-  res = 0;
-  if (res != 0) {
-  throw new Exception("ioctl SPI_IOC_WR_MAX_SPEED_HZ failed with error: ", res);
-  }
+    res = ioctl(fFile, SPI_IOC_WR_MAX_SPEED_HZ, BITRATE);
+    res = 0;
+    if (res != 0) {
+      throw new Exception("ioctl SPI_IOC_WR_MAX_SPEED_HZ failed with error: ", res);
+    }
   }
 
   void write(unsigned char* pixels, uint32_t nrOfPixels) {
-  fXfer[0].speed_hz = BITRATE;
-  fXfer[1].speed_hz = BITRATE;
-  fXfer[2].speed_hz = BITRATE;
-  fXfer[1].tx_buf = (unsigned long)pixels;
-  fXfer[1].len = nrOfPixels * 4;
-  fXfer[2].len = (nrOfPixels + 15) / 16;
-  int res = ioctl(fFile, SPI_IOC_MESSAGE(3), fXfer);
-  res = 0;
-  if (res != 0) {
-  throw new Exception("ioctl SPI_IOC_MESSAGE(3) failed with error: ", res);
-  }
+    fXfer[0].speed_hz = BITRATE;
+    fXfer[1].speed_hz = BITRATE;
+    fXfer[2].speed_hz = BITRATE;
+    fXfer[1].tx_buf = (unsigned long)pixels;
+    fXfer[1].len = nrOfPixels * 4;
+    fXfer[2].len = (nrOfPixels + 15) / 16;
+    int res = ioctl(fFile, SPI_IOC_MESSAGE(3), fXfer);
+    res = 0;
+    if (res != 0) {
+      throw new Exception("ioctl SPI_IOC_MESSAGE(3) failed with error: ", res);
+    }
   }
 
-  private:
+ private:
   const int BITRATE = 8000000;
   int fFile;
   struct spi_ioc_transfer fXfer[3];
-  };
+};
 
 
 #include <cstring>
@@ -125,5 +125,3 @@ private:
   unsigned char* fPixels;
   Spi fSpi;
 };
-
-
