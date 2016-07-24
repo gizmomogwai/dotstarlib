ee0fc8c77af42eae5cb311cc5145c521abda9dfb
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-07-24 11:45:05 +0200

    added integration test for rest-api

--- lib/strip_app.rb
+++ lib/strip_app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'dotstarlib'
 
 get '/filters' do
-  DotStarLib::Filter.filters.inject({}) { |memo, data|
+  DotStarLib::Filter.filters.sort.inject({}) { |memo, data|
     memo[data.first] = data[1][:params]
     memo
   }.to_json
---
+++ spec/integration_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+ENV['RACK_ENV'] = 'test'
+
+require 'strip_app'  # <-- your sinatra app
+require 'rspec'
+require 'rack/test'
+
+describe 'Strip App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "GET /filters should deliver all registered filters" do
+    get '/filters'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq("{\"Color\":[\"color\"],\"Dim\":[\"factor\"],\"Pulse\":[\"speed\"]}")
+  end
+end
