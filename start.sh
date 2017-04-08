0557271d0c676ed6cd92549a9e670a5839265042
Christian Koestlin <christian.koestlin@gmail.com>
2017-04-08 08:43:50 +0200

    Use https to update from github

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "hello world"
 source /home/osmc/.rvm/scripts/rvm
 cd /home/osmc/dotstarlib
-git fetch origin
+git fetch https://github.com/gizmomogwai/dotstarlib.git
 git reset --hard origin/master
 cp settings.yaml.local settings.yaml
 bundle exec ruby -Ilib lib/strip_app.rb
