db5fd9204295e751d3b427f00e3a17073eb42e49
Christian Koestlin <christian.koestlin@gmail.com>
2017-04-08 08:40:53 +0200

    Update from origin and apply local settings before start

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 echo "hello world"
 source /home/osmc/.rvm/scripts/rvm
 cd /home/osmc/dotstarlib
+git fetch origin
+git reset --hard origin/master
+cp settings.yaml.local settings.yaml
 bundle exec ruby -Ilib lib/strip_app.rb
